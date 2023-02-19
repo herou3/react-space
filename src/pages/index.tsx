fe6bb519a095650fd019e3b3fe614df21c38bc55
herou3 <herou3@yandex.ru>
2023-02-20 01:12:01 +0700

    Added template for scr2; Added template for scr3

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -41,6 +41,13 @@ export default function Home() {
           </ul>
         </div>
       </section>
+      <section className={cl(styles.section, styles.sectionEarth)}>
+        <Title size='Large'>
+          Factions
+        </Title>
+      </section>
+      <section className={cl(styles.section, styles.sectionRoadMap)}>
+      </section>
     </div>
   )
 }
